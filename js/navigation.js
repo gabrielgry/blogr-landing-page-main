bb64254252294edc62009d32b84f7e67eccfd7b3
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2021-11-03 02:27:18 -0300

    Mobile navigation test

--- js/navigation.js
+++ js/navigation.js
@@ -1,55 +1,78 @@
-const hamburgerElement = document.getElementsByClassName("i-hamburger");
+const hamburger = document.getElementsByClassName("i-hamburger")[0];
+const navContainer = document.getElementsByClassName("nav__container")[0];
 const dropdownElements = document.getElementsByClassName("nav__dropdown");
 
 const dropdowns = [];
 
 for (let i = 0; i < dropdownElements.length; i++) {
-  const navLink = dropdownElements[i].querySelector(".nav__link");
-  const dropdownIcon = dropdownElements[i].querySelector(".dropdown__icon");
-  const dropdownSubnav = dropdownElements[i].querySelector(".dropdown__subnav");
-
   dropdowns.push({
     parent: dropdownElements[i],
-    dropdownIcon,
-    navLink,
-    dropdownSubnav,
+    dropdownIcon: dropdownElements[i].querySelector(".dropdown__icon"),
+    navLink: dropdownElements[i].querySelector(".nav__link"),
+    dropdownSubnav: dropdownElements[i].querySelector(".dropdown__subnav"),
     key: i,
+  });
+}
+
+function handleDropdownMobile() {
+  let activeDropdown = -1;
+  let prevActiveDropdown = -1;
+
+  for (let dropdown of dropdowns) {
+    dropdown.navLink.addEventListener("click", () => {
+      if (activeDropdown !== dropdown.key) {
+        prevActiveDropdown = activeDropdown;
+        activeDropdown = dropdown.key;
+        dropdown.dropdownSubnav.classList.add("dropdown__subnav--show");
+        dropdown.dropdownIcon.classList.add("i-arrow--active");
+      } else {
+        prevActiveDropdown = -1;
+        activeDropdown = -1;
+        dropdown.dropdownSubnav.classList.remove("dropdown__subnav--show");
+        dropdown.dropdownIcon.classList.remove("i-arrow--active");
+      }
+
+      if (prevActiveDropdown !== -1) {
+        dropdowns[prevActiveDropdown].dropdownSubnav.classList.remove(
+          "dropdown__subnav--show"
+        );
+        dropdowns[prevActiveDropdown].dropdownIcon.classList.remove(
+          "i-arrow--active"
+        );
+      }
+    });
+  }
+}
+
+function handleDropdownDesktop() {
+  for (let dropdown of dropdowns) {
+    dropdown.parent.addEventListener("mouseenter", () => {
+      dropdown.dropdownIcon.classList.add("i-arrow--active");
+    });
+    dropdown.parent.addEventListener("mouseleave", () => {
+      dropdown.dropdownIcon.classList.remove("i-arrow--active");
+    });
+  }
+}
+
+function handleHamburgerMenu() {
+  hamburger.addEventListener("click", () => {
+    navContainer.classList.toggle("nav__container--hidden");
+    hamburger.classList.toggle("i-close");
   });
 }
 
 function navigation(mobileQuery) {
   if (mobileQuery.matches) {
-    let activeDropdown = -1;
-    let prevActiveDropdown = -1;
-
-    for (let dropdown of dropdowns) {
-      dropdown.parent.addEventListener("click", () => {
-        if (activeDropdown !== dropdown.key) {
-          prevActiveDropdown = activeDropdown;
-          activeDropdown = dropdown.key;
-          dropdown.dropdownSubnav.classList.add("dropdown__subnav--show");
-          dropdown.dropdownIcon.classList.add("i-arrow--active");
-        } else {
-          prevActiveDropdown = -1;
-          activeDropdown = -1;
-          dropdown.dropdownSubnav.classList.remove("dropdown__subnav--show");
-          dropdown.dropdownIcon.classList.remove("i-arrow--active");
-        }
-
-        if (prevActiveDropdown !== -1) {
-          dropdowns[prevActiveDropdown].dropdownSubnav.classList.remove(
-            "dropdown__subnav--show"
-          );
-          dropdowns[prevActiveDropdown].dropdownIcon.classList.remove(
-            "i-arrow--active"
-          );
-        }
-      });
-    }
+    handleDropdownMobile();
+    handleHamburgerMenu();
   } else {
+    handleDropdownDesktop();
   }
 }
 
 var mobileQuery = window.matchMedia("(max-width: 800px)");
 navigation(mobileQuery);
 mobileQuery.addEventListener("change", navigation);
+
+//Hamburger menu
